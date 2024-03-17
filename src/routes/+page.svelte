dcff7a920b32da3a48398c3b6070125ef6644199
Shota Narisaka <shootacean@gmail.com>
2024-03-17 17:03:43 +0900

    create time sample

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,16 @@
-<button class="btn btn-primary">Hello daisyUI</button>
+<span class="countdown">
+  <span style="--value:{value};"></span>
+</span>
+
+<script>
+  let value = 5;
+  const timer = setInterval(() => {
+    value -= 1;
+  }, 1000);
+  setTimeout(() => {
+    clearInterval(timer);
+  }, 5000);
+</script>
 
 <style lang="postcss">
   :global(html) {
