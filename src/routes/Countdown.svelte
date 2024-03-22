cc911aaa008613927dba5ae88029f1c974b57d6a
Shota Narisaka <shootacean@gmail.com>
2024-03-22 22:04:46 +0900

    add send notification at timer finished

--- src/routes/Countdown.svelte
+++ src/routes/Countdown.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
   import { onMount } from 'svelte';
 
+
+  // TODO : 編集できるようにする
   export let title: string = 'Countdown';
   export let end: Date = new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate() + 1);
 
@@ -8,7 +10,10 @@
   let minutes = 0;
   let seconds = 0;
 
-  // 次の日の0時0分0秒までの残り時間を計算する関数
+  /**
+   * カウントダウンを計算する
+   * @returns void
+   */
   function calculateCountdown() {
     const now = new Date();
     const diff = end.getTime() - now.getTime(); // ミリ秒単位の差分
@@ -16,6 +21,24 @@
     hours = Math.floor(diff / 1000 / 60 / 60);
     minutes = Math.floor(diff / 1000 / 60) % 60;
     seconds = Math.floor(diff / 1000) % 60;
+
+    if (hours === 0 && minutes === 0 && seconds === 0) {
+      // タイマーの完了通知を送る
+      const message = `Finished : ${title}`
+      if (!("Notification" in window)) {
+        console.debug("This browser does not support desktop notification");
+      } else if (Notification.permission === 'granted') {
+        console.debug("notifiction already granted");
+        new Notification(message);
+      } else {
+        Notification.requestPermission().then((permission) => {
+          console.debug("notifiction request result", permission);
+          if (permission === 'granted') {
+            new Notification(message);
+          }
+        });
+      }
+    }
   }
 
   // コンポーネントがマウントされた後、1秒ごとにカウントダウンを更新
