acb8d1b94b7b61daaf4c053ed38442e27ddc604e
Shota Narisaka <shootacean@gmail.com>
2024-03-23 18:38:51 +0900

    refactor Lifespan

--- src/routes/Lifespan.svelte
+++ src/routes/Lifespan.svelte
@@ -20,13 +20,13 @@
 </script>
 
 <section>
-  <h2 class="text-left">Lifespan ( {LIFESPAN} )</h2>
+  <h2 class="text-left">Lifespan ( {LIFESPAN} years old )</h2>
   <progress class="progress progress-primary w-full" value={LIFESPAN-currentAge} max={LIFESPAN}></progress>
   <Countdown end={new Date(new Date().getFullYear() + LIFESPAN-currentAge, new Date().getMonth(), new Date().getDate())} />
 </section>
 
 <section class="mt-8">
-  <h2 class="text-left">Healthy Lifespan ( {HEALTHY_LIFESPAN} )</h2>
+  <h2 class="text-left">Healthy Lifespan ( {HEALTHY_LIFESPAN} years old )</h2>
   <progress class="progress progress-success w-full" value={HEALTHY_LIFESPAN-currentAge} max={HEALTHY_LIFESPAN}></progress>
   <Countdown end={new Date(new Date().getFullYear() + HEALTHY_LIFESPAN-currentAge, new Date().getMonth(), new Date().getDate())} />
 </section>
